90a5440b054166c1a65db0b8176ed8491fdccf78
Graham French <Graham.French@jncc.gov.uk>
2019-01-24 12:04:07 +0000

    added create_reporting_tool_enviroment function

---
+++ R/database.R
@@ -0,0 +1,26 @@
+#' Create reporting tool environment
+#'
+#' This function creates the reporting tool environment used to
+#' store the id values required as primary and foreign keys in
+#' the reporting tool database
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+create_reporting_tool_environment <- function() {
+  
+  if (!exists("reporting_tool", mode = "environment")) {
+    assign("reporting_tool", new.env(parent = emptyenv()), envir = globalenv())
+    
+    # assign values in environment
+    assign("latest_bird_id ", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_annex_ii_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_change_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_measure_info_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_measure_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_pressure_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_pressure_info_id", 0L, envir = reporting_tool, inherits = FALSE)
+    assign("latest_notes_id", 0L, envir = reporting_tool, inherits = FALSE)
+  }
+}
