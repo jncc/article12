3693e2e20e25d6072f8f3dffeded52c9b67b5d35
Graham French <Graham.French@jncc.gov.uk>
2019-05-14 09:50:52 +0100

    updated information_species_look_up

--- data-raw/lookup_raw.R
+++ data-raw/lookup_raw.R
@@ -2,9 +2,13 @@
 url <- "http://biodiversity.eionet.europa.eu/activities/Reporting/Article_12/Reports_2019/Files_2019/art12_checklist_v2_update20180616.xls"
 dest_file <- "art12_checklist_v2_update20180616.xls"
 curl::curl_download(url, dest_file)
-information_species_look_up <- readxl::read_excel(dest_file, sheet = "code list") %>% 
+information_species_look_up <- readxl::read_excel(dest_file, sheet = "Art12 checklist") %>% 
   janitor::clean_names() %>% 
+  dplyr::filter(country == "UK") %>% 
   dplyr::rename(speciescode = species_code,
                 speciesname = species_name) %>% 
-  dplyr::select(speciescode:euringcode) %>% 
+  dplyr::select(speciescode:recommended_unit) %>% 
   dplyr::arrange(speciesname, sub_unit)
+
+
+
