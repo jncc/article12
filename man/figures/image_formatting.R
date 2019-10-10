59d75bf52bdce23864d5ca219a7409c084680381
Graham French <Graham.French@jncc.gov.uk>
2019-10-10 14:50:28 +0100

    updated logo

--- man/figures/image_formatting.R
+++ man/figures/image_formatting.R
@@ -16,7 +16,7 @@ mask <- image_read("./man/figures/mask.png") %>%
 
 image <- image_read("./man/figures/flag_yellow_high.jpg") %>%
   image_convert(format = "png") %>% 
-  image_crop("1575x1575+394+20") #widthxheight+x+y
+  image_crop("1575x1575+394+20") #<width>x<height>{+-}<xoffset>{+-}<yoffset>
   
 image <- image_composite(mask, image, "plus") %>%
   image_transparent("white", fuzz = 20L)
@@ -25,14 +25,14 @@ image <- image_composite(mask, image, "plus") %>%
 image %>% 
   image_annotate("article12", size = 150L, gravity = "center", 
                  color = "#FFCC00", style = "italic",
-                 strokecolor = "#9E8508",
-                  font = "sans") %>% 
+                 strokecolor = "#9E8508",  # 9E8508 = European star yellow
+                 font = "sans") %>% 
   image_write(path = "./man/figures/eu_flag.png", format = "png")
 
 # create hex sticker logo
 sticker(subplot = "./man/figures/eu_flag.png", s_x = 1, s_y = 1, s_width = 1, s_height = 1, 
         package = NA_character_,
-        h_fill = "#003399", h_color = NA_character_,
+        h_fill = "#003399", h_color = NA_character_, #003399 = European flag blue
         asp = 0.8,
         filename = "man/figures/logo_1.png")
 
@@ -54,6 +54,28 @@ image_read("./man/figures/logo_3.png") %>%
   image_fill("white", "+200+580") %>%
   image_fill("white", "+200+10") %>%
   image_transparent("white") %>% 
+  image_write(path = "./man/figures/logo_4.png", format = "png")
+
+# format JNCC logo
+jncc <- image_read("./man/figures/jncc.jpg") %>% 
+  image_convert(format = "png") %>% 
+  image_rotate(330L) %>%
+  image_threshold(type = "white", threshold = "70%") %>%
+  image_threshold(type = "black", threshold = "30%") %>%
+  image_transparent("white") %>%  
+  image_fill("#9EB7F0", "+200+500") %>%
+  image_fill("#9EB7F0", "+400+500") %>%
+  image_fill("#9EB7F0", "+475+400") %>%
+  image_fill("#9EB7F0", "+600+350") %>%
+  image_fill("#9EB7F0", "+750+250") %>%
+  image_scale("78x100") %T>% 
+  image_write(path = "./man/figures/jncc.png", format = "png")
+
+# add jncc logo
+image_read("./man/figures/logo_4.png") %>% 
+  image_composite(jncc, offset = "+430+420") %>% 
   image_write(path = "./man/figures/logo.png", format = "png")
+
+
  
  
